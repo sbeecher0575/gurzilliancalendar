a3054938cff483288dea1e9e9fcc147ddd3b5ea0
sbeecher0575 <sbeecher0575@gmail.com>
2022-02-09 23:13:25 -0700

    qordle update

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const fs = require('fs')
+const util = require('./wordUtil.js')
+util.initLists();
 const app = express()
 const port = 3000
-
+app.use(express.static(__dirname + '/qordle'));
+app.get('/qordle/api', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin','*')
+	if(req.query.ask=="request"){
+		debugger
+		var randVals = util.getRandomWords(req.query.count)
+		console.log(randVals)
+		res.send(randVals)
+	} else if(req.query.ask=="check"){
+		debugger
+		var dat = []
+		dat.push(util.isValid(req.query.word))
+		res.send(dat)
+		console.log(dat)
+	}
+})
+app.use(express.static(__dirname + '/qordle'));
+app.get('/qordle',(req,res)=>{
+	res.setHeader('Access-Control-Allow-Origin','*')
+	res.sendFile(__dirname + "/qordle/index.html",(err) => {
+		if(err){
+			console.log(err);
+			res.end(err.message);
+		}
+	});
+})
 app.use(express.static(__dirname + '/'));
 app.get('/',(req,res)=>{
 	res.setHeader('Access-Control-Allow-Origin','*')
@@ -13,6 +40,9 @@ app.get('/',(req,res)=>{
 		}
 	});
 })
+
+
+
 app.use(express.static(__dirname + '/'));
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
